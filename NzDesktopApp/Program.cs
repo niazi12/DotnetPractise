b2c38970c3492ba90a3362179d372ea77d65c23b
Niazi Mahrab <97739823@rave.ac.uk>
2024-02-09 12:33:04 +0000

    dd

---
+++ NzDesktopApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NzDesktopApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            
+            int myNum1 = 15;
+
+            int myNum2 = 10;
+
+            int sum = myNum1 + myNum2;
+            int sub = myNum1 - myNum2;
+            Console.WriteLine("Sum is", sum);
+            Console.WriteLine(sub);
+            Console.Read();
+
+        }
+    }
+}
